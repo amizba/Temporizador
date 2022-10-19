f0b7d19d744a979e35a9902247d7764ca93a0bc1
amizba <usuario@DESKTOP-05358B1>
2022-10-19 19:20:11 +0200

    Creación de un temporizador que te muestra la hora cada cinco segundos por consola.

---
+++ src/Temporizador.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+import java.awt.event.*;
+
+import java.util.*;
+
+import javax.swing.Timer;
+
+public class Temporizador {
+
+	public static void main(String[] args) {
+		
+		DameLaHora oyente=new DameLaHora();
+		
+		//instanciamos la clase Timer y creación del objeto mitemporizador
+		Timer mitemporizador=new Timer(5000, oyente); 
+		
+		mitemporizador.start();
+		
+		JOptionPane.showMessageDialog(null, "Pulsa Aceptar para detener");
+		
+		System.exit(0);
+
+	}
+
+}
+class DameLaHora implements ActionListener{
+	
+	public void actionPerformed(ActionEvent e) {
+
+			
+			Date ahora=new Date();
+			
+			System.out.println("Te pongo la hora cada 5 sg: "+ahora);
+		}
+	}
+
+
